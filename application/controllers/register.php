dff150db94350b2a7adb549db19301f4f91524e7
bisho1995 <pritammukherjee16@gmail.com>
2018-02-18 07:57:42 +0530

    fixed internal server errors caused by

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -31,13 +31,13 @@ class Register extends CI_Controller
 	private function form_validations()
 	{
 		$this->form_validation->set_rules('fullname','Full Name','required|min_length[4]|max_length[20]');
-		$this->form_validation->set_rules('email','Email','required|min_length[4]|max_length[20]|valid_email');
+		$this->form_validation->set_rules('email','Email','required|min_length[4]|max_length[50]|valid_email|is_unique[teams.email]');
 		$this->form_validation->set_rules('teamname','Team Name','required|min_length[4]|max_length[20]');
 		$this->form_validation->set_rules('password','Team Name','required|min_length[4]|max_length[20]');
 		$this->form_validation->set_rules('confirm_password','Team Name','required|min_length[4]|max_length[20]|matches[password]');
 		if($this->form_validation->run()== FALSE)
 		{
-			$this->display_error_message_as_form_is_not_validated();
+			$this->send_error_status_to_sender(validation_errors('',''));
 		}
 		else
 		{
@@ -46,16 +46,39 @@ class Register extends CI_Controller
 
 	}
 
-	private function display_error_message_as_form_is_not_validated()
+	private function send_error_status_to_sender($errors='')
 	{
-		echo json_encode(array("error",validation_errors('<span>','</span>')));
+		echo json_encode(array("error" => array($errors)));
+	}
+
+	private function send_success_status_to_sender($success='')
+	{
+		echo json_encode(array("success" => array($success)));
 	}
 
 	private function proceed_to_add_data_to_database()
 	{
 		$this->db->trans_start();
-		
+		$this->add_team_data_to_database();
 		$this->db->trans_complete();
+		if($this->db->trans_status() == FALSE)
+		{
+			$this->send_error_status_to_sender('Sorry there was some problem with the registration.');
+		}
+		else
+		{
+			$this->send_success_status_to_sender('The team has been registered successfully.');
+		}
+	}
+	private function add_team_data_to_database()
+	{
+		$data=array(
+			'full_name' => $this->input->post('fullname'),
+			'email' => $this->input->post('email'),
+			'team_name'=> $this->input->post('teamname'),
+			'password' => $this->input->post('password')
+		);
+		$this->db->insert('teams',$data);
 	}
 }
 ?>
