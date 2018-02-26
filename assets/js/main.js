72885a234767dab5fb6639faa1d7abe7536e4fcf
bisho1995 <pritammukherjee16@gmail.com>
2018-02-26 19:51:32 +0530

    modified base url of app had a trailing / which was causing inconsistency so removed it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', []);
 
 appData={
-	url: "http://localhost:82/201802/CodeIgniter-3.1.7/CodeIgniter-3.1.7/"
+	url: "http://localhost:82/201802/CodeIgniter-3.1.7/CodeIgniter-3.1.7"
 };
 
 function redirect(href)
